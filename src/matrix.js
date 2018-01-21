302fd0326dbce99e2e186c15cde04c4c45ed05ee
wu kai <wukai2048@gmail.com>
2018-01-21 17:51:50 +0800

    每次移动时随机添加一个数字

--- src/matrix.js
+++ src/matrix.js
@@ -1,17 +1,45 @@
 export default class Matrix {
     constructor({ matrix }) {
         this.matrix = matrix;
+        this.addRandomNumToMatrix();
     }
 
-    getEmptyCoordinates = () => {}
+    /**
+     * 获取空白格子的坐标
+     */
+    getEmptyCoordinates = () => {
+        const { matrix } = this;
+        const coordinates = [];
 
-    getRandom = arr => {}
+        matrix.forEach((row, i) => {
+            row.forEach((value, j) => {
+                if (value === 0) {
+                    coordinates.push([i, j]);
+                }
+            });
+        });
 
-    isBoardMoved = (preMatrix, newMatrix) => {}
+        return coordinates;
+    };
 
-    checkGameOver = matrix => {}
+    getRandom = arr => {
+        return arr[Math.floor(Math.random() * arr.length)];
+    };
 
-    addRandomNumToMatrix = () => {}
+    isBoardMoved = (preMatrix, newMatrix) => {};
+
+    checkGameOver = matrix => {};
+
+    addRandomNumToMatrix = () => {
+        const emptyCoords = this.getEmptyCoordinates();
+        if (emptyCoords.length === 0) {
+            return;
+        }
+
+        const [row, col] = this.getRandom(emptyCoords);
+        const newMatrix = this.matrix;
+        newMatrix[row][col] = this.getRandom([2, 4]);
+    };
 
     rotateRight = () => {
         const { matrix } = this;
@@ -26,7 +54,7 @@ export default class Matrix {
             }
         }
         this.matrix = newMatrix;
-    }
+    };
 
     rotateLeft = () => {
         const { matrix } = this;
@@ -40,59 +68,68 @@ export default class Matrix {
             }
         }
         this.matrix = newMatrix;
-    }
+    };
 
     shiftRight = () => {
         const { matrix } = this;
         const newMatrix = matrix.map(row => {
             const newRow = [];
             row.forEach(v => {
-                if (v === 0) newRow.unshift(0); 
+                if (v === 0) newRow.unshift(0);
                 else newRow.push(v);
             });
             return newRow;
         });
         this.matrix = newMatrix;
-    }
+    };
 
     shiftLeft = () => {
         const { matrix } = this;
         const newMatrix = matrix.map(row => {
             const newRow = [];
             row.reverse().forEach(v => {
-                if (v === 0) newRow.push(0); 
+                if (v === 0) newRow.push(0);
                 else newRow.unshift(v);
             });
             return newRow;
-        })
+        });
         this.matrix = newMatrix;
-    }
+    };
 
-    combineNumToLeft = () => {}
+    combineNumToLeft = () => {};
 
-    combineNumToRight = () => {}
-    
-    move = callback => {}
+    combineNumToRight = () => {};
+
+    move = callback => {
+        callback();
+        this.addRandomNumToMatrix();
+    };
 
     moveUp = () => {
-        this.rotateRight();
-        this.shiftRight();
-        this.rotateLeft();
-    }
+        this.move(() => {
+            this.rotateRight();
+            this.shiftRight();
+            this.rotateLeft();
+        });
+    };
 
     moveDown = () => {
-        this.rotateLeft();
-        this.shiftRight()
-        this.rotateRight();
-    }
+        this.move(() => {
+            this.rotateLeft();
+            this.shiftRight();
+            this.rotateRight();
+        });
+    };
 
     moveLeft = () => {
-        this.shiftLeft();
-    }
+        this.move(() => {
+            this.shiftLeft();
+        });
+    };
 
     moveRight = () => {
-        this.shiftRight();
-    }
+        this.move(() => {
+            this.shiftRight();
+        });
+    };
 }
-
-
