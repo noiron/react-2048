875f39c64a962d58d46991f91023ffaafe2da317
wu kai <wukai2048@gmail.com>
2018-01-21 21:53:01 +0800

    修改样式，格子显示不同颜色

--- src/components/Cell/index.js
+++ src/components/Cell/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './cell.css';
 
 
 export default class Cell extends React.Component {
@@ -10,11 +11,11 @@ export default class Cell extends React.Component {
     render() {
         const { value } = this.props;
 
-        const gray = Math.max(255 - Math.floor(value * 2048 / 255), 100);
-        const style = `rgba(${gray}, ${gray}, ${gray}, 0.6)`;
+        // const gray = Math.max(255 - Math.floor(value * 2048 / 255), 100);
+        // const style = `rgba(${gray}, ${gray}, ${gray}, 0.6)`;
 
         return (
-            <td style={{ backgroundColor: style }}>
+            <td className={`color-${value} cell` }>
                 <div>{value || null}</div>
             </td>
         )
