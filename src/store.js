b04328bc51db320ccceeaea472fd110ab6487386
wu kai <wukai2048@gmail.com>
2018-01-31 20:12:15 +0800

    [fixbug]当localstorage中为空时无法打开

--- src/store.js
+++ src/store.js
@@ -4,16 +4,16 @@ import undoable, { excludeAction } from 'redux-undo';
 
 import game from './reducers';
 
-const initialHistory = JSON.parse(localStorage.getItem('2048_game_state') || null);
+const initialHistory = JSON.parse(localStorage.getItem('2048_game_state') || 'null');
 
 const undoBoardReducer = undoable(game, {
     limit: 10,
     // ignoreInitialState: false,
     filter: excludeAction('INIT'),
-})
+});
 
 export const store = createStore(
     undoBoardReducer,
-    initialHistory,
+    // initialHistory,
     devToolsEnhancer()
 );
